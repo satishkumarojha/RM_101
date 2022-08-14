b991908d41e356e6fc049f41624ff80c5b9beee8
satishkumarojha <satishkumarojha15@gmail.com>
2022-08-14 15:11:02 +0530

    debouncing done

--- debounce/index.js
+++ debounce/index.js
@@ -4,11 +4,9 @@ const debounce = (timer)=>{
     if(id){
         clearTimeout(id)
     }
-    else{
         id=setTimeout(()=>{
             let h1 = document.createElement('h1')
             h1.innerText="Debounce text displayed after 3sec";
             debouncediv.append(h1);
         },timer);
-    }
 }
